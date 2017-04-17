8e7d230028f1e8fe3e8c50cbd006c3e67576d770
Denis Azhymov <azhimov93@gmail.com>
2017-04-17 15:22:37 +0300

    add new materials from chapter5

---
+++ src/main/java/com/getfire/spring/ch5/aop/helloWorld/HelloWorldAOPExample.java
@@ -0,0 +1,17 @@
+package com.getfire.spring.ch5.aop.helloWorld;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+public class HelloWorldAOPExample {
+    public static void main(String[] args) {
+        MessageWriter target = new MessageWriter();
+        ProxyFactory pf = new ProxyFactory();
+        pf.addAdvice(new MessageDecorator());
+        pf.setTarget(target);
+
+        MessageWriter proxy = (MessageWriter)pf.getProxy();
+        target.writeMessage();
+        System.out.println("");
+        proxy.writeMessage();
+    }
+}
---
+++ src/main/java/com/getfire/spring/ch5/aop/helloWorld/MessageWriter.java
@@ -0,0 +1,7 @@
+package com.getfire.spring.ch5.aop.helloWorld;
+
+public class MessageWriter {
+    public void writeMessage(){
+        System.out.print("World");
+    }
+}
